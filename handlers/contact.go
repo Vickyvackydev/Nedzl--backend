90830cc0784fe0a2eb93d20c72a79afb32fdac28
Vickyvackydev <vickyvacky5@gmail.com>
2026-01-01 06:34:24 -0500

    update:: added the delete api and get contacts api

--- handlers/contact.go
+++ handlers/contact.go
@@ -5,6 +5,7 @@ import (
 	"api/utils"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
@@ -25,4 +26,34 @@ func Contact(db *gorm.DB) echo.HandlerFunc {
 		return utils.ResponseSucess(c, http.StatusOK, "Contact mail created successfully", nil)
 	}
 
+}
+
+func GetContact(db *gorm.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var contact models.Contact
+
+		if err := db.Find(&contact).Error; err != nil {
+			return utils.ResponseError(c, 500, "Failed to retrieve contact lists", err)
+
+		}
+
+		return utils.ResponseSucess(c, http.StatusOK, "Contact lists retrieved successfully", contact)
+	}
+}
+
+func DeleteContact(db *gorm.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ContactId := c.Param("id")
+
+		Cuid, err := uuid.Parse(ContactId)
+		if err != nil {
+			return utils.ResponseError(c, http.StatusBadRequest, "Invalid contact ID", err)
+		}
+
+		if err := db.Model(&models.Contact{}).Where("id = ?", Cuid).Delete(&models.Contact{}).Error; err != nil {
+			return utils.ResponseError(c, http.StatusInternalServerError, "Failed to delete contact mail", err)
+		}
+
+		return utils.ResponseSucess(c, http.StatusOK, "Contact mail deleted successfully", nil)
+	}
 }
