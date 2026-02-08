2acbe8d513f7990c6e24c9f21ad106a69f24866e
Vickyvackydev <vickyvacky5@gmail.com>
2026-02-08 00:51:51 -0500

    fix:: fixed 500 internal server error

--- utils/find_similar_product.go
+++ utils/find_similar_product.go
@@ -15,24 +15,25 @@ func FindSimilarProducts(db *gorm.DB, product *models.Products, limit int) ([]mo
 
 	keywords := strings.Fields(strings.ToLower(product.Name))
 
-	query := db.Model(&products).Where("product_name = ?", product.Name).Where("id != ?", product.ID).Where("status =?", models.StatusOngoing)
+	query := db.Model(&products).Where("product_name = ?", product.Name).Where("id != ?", product.ID).Where("status = ?", models.StatusOngoing)
 
 	// we apply the key word similarity
 
 	keywordQuery := db
 
 	for _, kw := range keywords {
-		keywordQuery = keywordQuery.Or("product_name ILIKE ? OR description ILIKE ?", "%"+kw+"%", "%"+kw+"%")
+		keywordQuery = keywordQuery.Or("name ILIKE ? OR description ILIKE ?", "%"+kw+"%", "%"+kw+"%")
 	}
 
 	query = query.Where(keywordQuery)
 
 	// ranking the results by relevance (number of matching keywords)
 
-	query = query.Order(`CASE
-	WHEN product_name ILIKE '%` + keywords[0] + `%' THEN 1
-	WHEN description ILIKE '%` + keywords[0] + `%' THEN 1
-	ELSE 3
+	query = query.Order(`
+	CASE
+		WHEN name ILIKE '%` + keywords[0] + `%' THEN 1
+		WHEN description ILIKE '%` + keywords[0] + `%' THEN 2
+		ELSE 3
 	 END,
 	 created_at DESC`)
 
